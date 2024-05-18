59b253e5623b0d430c917b0f6c64c5bf3747fc30
David Becerra <davidnbr_98@hotmail.com>
2024-05-18 17:37:56 -0500

    Update MathPairs.py

--- technical_preparation/MathPairs.py
+++ technical_preparation/MathPairs.py
@@ -1,25 +1,20 @@
+# Find the pairs of numbers that can give a certain result with their sum. Given an array of numbers and a k result.
+
 def pairs(k, arr):
     count = 0
-    # arr.append(arr[0])
+
     for i in range(len(arr)):
-        # print(i,arr[arr.index(i):])
-        # print(arr[i],arr[i+1:])
-        # other_arr = arr[arr.index(i):]
-        # for j in arr[arr.index(i):]:
         if i == (len(arr) - 1):
             break
         else:
             for j in range(i, len(arr)):
                 if abs(arr[i] - arr[j]) == k:
-                    # print(arr[i],arr[j])
-                    # print(abs(arr[i]-arr[j]))
                     count += 1
 
     return count
 
 
 if __name__ == "__main__":
-    # fptr = open(os.environ["OUTPUT_PATH"], "w")
 
     first_multiple_input = input().rstrip().split()
 
@@ -30,5 +25,3 @@ if __name__ == "__main__":
     arr = list(map(int, input().rstrip().split()))
 
     result = pairs(k, arr)
-
-    # fptr.write(str(result) + "\n")
